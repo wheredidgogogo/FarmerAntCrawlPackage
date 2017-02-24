f1c5bef46ea01f5fd90b330a8af3117ae36a8a82
Casper Lai <casper.lai@sleepingdesign.com>
2017-02-24 17:16:12 +0800

    Add request api

--- src/FarmerAntCrawler.php
+++ src/FarmerAntCrawler.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace Farmerant\Farmerantcrawl;
+use Farmerant\Farmerantcrawl\Listing\Product;
+use GuzzleHttp\Client;
+use Illuminate\Support\Collection;
 
 /**
  * Class FarmerAntCrawler
@@ -14,11 +17,59 @@ class FarmerAntCrawler
     private $token;
 
     /**
+     * @var Collection
+     */
+    private $products;
+
+    /**
+     * @var Client
+     */
+    private $client;
+
+    /**
      * FarmerAntCrawler constructor.
      * @param $token
+     * @param $url
      */
-    public function __construct($token)
+    public function __construct($token, $url)
     {
         $this->token = $token;
+        $this->client = new Client(['base_uri' => $url]);
+    }
+
+    /**
+     * Add products
+     *
+     * @param Product $product
+     */
+    public function add(Product $product)
+    {
+        $this->products->push($product);
+    }
+
+    /**
+     * Send all products
+     */
+    public function sendAllProducts()
+    {
+        $this->products->each(function ($product) {
+           $this->request($product);
+        });
+    }
+
+    /**
+     * Request to api
+     *
+     * @param Product $product
+     * @return mixed|\Psr\Http\Message\ResponseInterface
+     */
+    private function request(Product $product)
+    {
+        return $this->client->request('POST', '/api/crawl', [
+            'header' => [
+                'Authorization' => $this->token,
+            ],
+            'json' => $product->toArray(),
+        ]);
     }
 }
